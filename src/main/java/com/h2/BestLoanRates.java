61e5a6d030b7ae68344f775e0d2f5f0daee60234
Michael MacDougall <mgmacdougall@outlook.com>
2023-02-26 12:37:58 -0500

    Add of the bestRates map

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -1,8 +1,12 @@
 package com.h2;
 
+import java.util.Map;
+
 public class BestLoanRates {
 
+    public static Map<Integer, Float> bestRates = Map.of(1, 5.50f, 2, 3.45f, 3, 2.67f);
+
     public static void main(String[] args) {
-        
+
     }
 }
