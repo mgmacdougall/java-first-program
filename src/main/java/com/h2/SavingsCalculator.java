81cbee314f47f456646b8d8b7d0038439c1307fe
Michael MacDougall <mgmacdougall@outlook.com>
2023-02-26 14:00:43 -0500

    Add of the main method

--- src/main/java/com/h2/SavingsCalculator.java
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -1,12 +1,35 @@
 package com.h2;
+
 import java.time.LocalDate;
 import java.time.YearMonth;
 
 public class SavingsCalculator {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args){
+
+        String[] creditsAsString= args[0].split(",");
+        String[] debitsAsString = args[1].split(",");
+
+
+        float[] credits = new float[creditsAsString.length];
+        float[] debits = new float[creditsAsString.length];
+
+        for(int i=0; i<creditsAsString.length; i++){
+            credits[i]= Float.parseFloat(creditsAsString[i]);
+        }
+
+        for(int i=0; i<creditsAsString.length; i++){
+            debits[i]= Float.parseFloat(debitsAsString[i]);
+        }
+
+        SavingsCalculator calculator = new SavingsCalculator(credits, debits);
+
+        float netSavings = calculator.calculate();
+
+        System.out.println("Net Savings = " + netSavings + ", remaining days in month = " + remainingDaysInMonth(LocalDate.now()));
+
     }
+
     private float[] credits;
     private float[] debits;
 
