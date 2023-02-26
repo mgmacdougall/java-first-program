a4c31ae46c0e7b2d78e048fd4c999b17a0ace543
Michael MacDougall <mgmacdougall@outlook.com>
2023-02-26 16:24:29 -0500

    Add of class members

--- src/main/java/com/h2/MortgageCalculator.java
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -1,4 +1,13 @@
 package com.h2;
 
 public class MortgageCalculator {
+
+    private long loanAmount;
+    private int termInYears;
+
+    private float annualRate;
+
+    private double monthlyPayment;
+
+    
 }
