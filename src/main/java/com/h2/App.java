45bcc3a9a9cf7c990d95354270472106f7860240
Michael MacDougall <mgmacdougall@outlook.com>
2023-02-26 12:57:15 -0500

    Add of main method

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -1,29 +1,27 @@
 package com.h2;
 
-import java.util.Scanner;
-
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello" );
-        Scanner scanner  = new Scanner(System.in);
-        System.out.println("Enter the loan term (in years)");
-        int loanTermInYears = scanner.nextInt();
-        float bestRate = BestLoanRates.getRates(loanTermInYears);
-
-        if(bestRate==0.0f){
-            System.out.println("No available rates for term: " + loanTermInYears + " years");
-        }else{
-            System.out.println("Best Available Rate: " + BestLoanRates.getRates(loanTermInYears) + "%");
-        }
-        scanner.close();
-
-    }
+//    public static void main( String[] args )
+//    {
+//        System.out.println( "Hello" );
+//        Scanner scanner  = new Scanner(System.in);
+//        System.out.println("Enter the loan term (in years)");
+//        int loanTermInYears = scanner.nextInt();
+//        float bestRate = BestLoanRates.getRates(loanTermInYears);
+//
+//        if(bestRate==0.0f){
+//            System.out.println("No available rates for term: " + loanTermInYears + " years");
+//        }else{
+//            System.out.println("Best Available Rate: " + BestLoanRates.getRates(loanTermInYears) + "%");
+//        }
+//        scanner.close();
+//
+//    }
 
     public static int doubleTheNumber(int number) {
         return number*2;
--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -1,6 +1,7 @@
 package com.h2;
 
 import java.util.Map;
+import java.util.Scanner;
 
 public class BestLoanRates {
 
@@ -12,5 +13,20 @@ public class BestLoanRates {
         }
         return 0.0f;
     }
-  
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello" );
+        Scanner scanner  = new Scanner(System.in);
+        System.out.println("Enter the loan term (in years)");
+        int loanTermInYears = scanner.nextInt();
+        float bestRate = BestLoanRates.getRates(loanTermInYears);
+
+        if(bestRate==0.0f){
+            System.out.println("No available rates for term: " + loanTermInYears + " years");
+        }else{
+            System.out.println("Best Available Rate: " + BestLoanRates.getRates(loanTermInYears) + "%");
+        }
+        scanner.close();
+
+    }
 }
